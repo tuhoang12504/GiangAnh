87e5b1a08caaa5884cf750b8a7b1a0b270c1586b
tuhoang12504 <139228578+tuhoang12504@users.noreply.github.com>
2023-09-24 00:46:23 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Đôi lời từ anh quân y của em",
+    introTitle: 'Giang Dao',
+    introDesc: `Có lần a bảo e là vua thương tích,nhớ không.
+    Vậy em có biết e khác với vua ở điểm nào không?
+    Đó là: vua thì "thường anh minh",
+    còn em thì ngược lại: "mình anh thương"🥰.`,
+    btnIntro: '^^Èo^^',
+    title: 'Đồng ý làm hậu phương cho anh nha 🥰',
+    desc: 'Lỡ thích người ta mất ròi á',
+    btnYes: 'Em Đồng Ý <33',
+    btnNo: 'Không Nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại chọn anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Hỏi nhiều zị cho chơi một mình luôn đóa  <33333333333',
+    mess: 'Thế thôi không hỏi nữa 🥰. Yêu em nhìu nhìu 😘😘',
+    messDesc: 'Trung thu này đi chơi với anh nhá',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/tuhoang12504'
+}
